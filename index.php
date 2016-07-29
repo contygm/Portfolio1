97192f2f6a8961e6e05f45bcfb49b84ad90649fb
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-07-29 15:14:45 -0400

    now I feel stupid

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-<?php include_once("assignment1/index.html"); ?>
-<?php include_once("assignment1/contact.html"); ?>
-<?php include_once("assignment1/portfolio.html"); ?>
+<?php include_once("index.html"); ?>
 
-<?php include_once("assignment2/index.html"); ?>
-<?php include_once("assignment2/contact.html"); ?>
-<?php include_once("assignment2/portfolio.html"); ?>

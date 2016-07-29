cd31574806987276863880f12b28cc8e7fd3368f
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-07-29 15:04:14 -0400

    trying to deploy4

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php include_once("assignment1/index.html"); ?>
 <?php include_once("assignment1/contact.html"); ?>
 <?php include_once("assignment1/portfolio.html"); ?>
+
+<?php include_once("assignment2/index.html"); ?>
+<?php include_once("assignment2/contact.html"); ?>
+<?php include_once("assignment2/portfolio.html"); ?>

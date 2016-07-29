c8f1e024b3fdaccf5ae0153dc89f1938280af4cb
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-07-29 14:59:41 -0400

    trying to deploy3

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php include_once("assignment1/index.html"); ?>
 <?php include_once("assignment1/contact.html"); ?>
 <?php include_once("assignment1/portfolio.html"); ?>
-<?php include_once("assignment1/assets/css/style.css"); ?>

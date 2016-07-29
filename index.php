f10db59322fa69ba4fb3b56be04c14da8fc19555
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-07-29 14:58:45 -0400

    trying to deploy2

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include_once("assignment1/index.html"); ?>
 <?php include_once("assignment1/contact.html"); ?>
-<?php include_once("assignment2/index.html"); ?>
+<?php include_once("assignment1/portfolio.html"); ?>
+<?php include_once("assignment1/assets/css/style.css"); ?>
